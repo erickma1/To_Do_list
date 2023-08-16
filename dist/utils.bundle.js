ae57ffea3691973161e46722a2378b2e3fce6275
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-08-16 20:19:27 +0000

    update bundle file

--- dist/utils.bundle.js
+++ dist/utils.bundle.js
@@ -10,51 +10,20 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/modules/task.js":
-/*!*****************************!*\
-  !*** ./src/modules/task.js ***!
-  \*****************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Task)\n/* harmony export */ });\nclass Task {\r\n  constructor(index, description) {\r\n    this.index = index;\r\n    this.description = description;\r\n    this.completed = false;\r\n  }\r\n}\r\n\n\n//# sourceURL=webpack://to_do_list/./src/modules/task.js?");
-
-/***/ }),
-
 /***/ "./src/modules/ul.js":
 /*!***************************!*\
   !*** ./src/modules/ul.js ***!
   \***************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   createToDoList: () => (/* binding */ createToDoList),\n/* harmony export */   renderToDoList: () => (/* binding */ renderToDoList)\n/* harmony export */ });\n/* harmony import */ var _task_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./task.js */ \"./src/modules/task.js\");\n\r\n\r\nfunction createToDoList() {\r\n  const todoList = [];\r\n\r\n  for (let i = 0; i < 5; i += 1) {\r\n    if (i === 1) todoList.push(new _task_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"](i, ` To Do ${i + 1}`, true));\r\n    else todoList.push(new _task_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"](i, `To Do ${i + 1}`, false));\r\n  }\r\n\r\n  return (todoList);\r\n}\r\n\r\nfunction renderToDoList(taskList) {\r\n  const ulElement = document.createElement('ul');\r\n\r\n  taskList.forEach((task) => {\r\n    const liElement = document.createElement('li');\r\n    const isChecked = 'unchecked';\r\n    liElement.innerHTML = `\r\n        <div>\r\n            <input type=\"checkbox\" ${isChecked}>\r\n            ${task.description}\r\n        </div>\r\n        <i class=\"fa fa-trash icon\" aria-hidden=\"true\"></i>\r\n        `;\r\n    ulElement.appendChild(liElement);\r\n  });\r\n\r\n  return ulElement;\r\n}\n\n//# sourceURL=webpack://to_do_list/./src/modules/ul.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ addEventListeners)\n/* harmony export */ });\n// import Task from './task.js';\r\n\r\n// export function createToDoList() {\r\n//   const todoList = [];\r\n\r\n//   for (let i = 0; i < 5; i += 1) {\r\n//     if (i === 1) todoList.push(new Task(i, ` To Do ${i + 1}`, true));\r\n//     else todoList.push(new Task(i, `To Do ${i + 1}`, false));\r\n//   }\r\n\r\n//   return (todoList);\r\n// }\r\n\r\n// export function renderToDoList(taskList) {\r\n//   const ulElement = document.createElement('ul');\r\n\r\n//   taskList.forEach((task) => {\r\n//     const liElement = document.createElement('li');\r\n//     const isChecked = 'unchecked';\r\n//     liElement.innerHTML = `\r\n//         <div>\r\n//             <input type=\"checkbox\" ${isChecked}>\r\n//             ${task.description}\r\n//         </div>\r\n//         <i class=\"fa fa-trash icon\" aria-hidden=\"true\"></i>\r\n//         `;\r\n//     ulElement.appendChild(liElement);\r\n//   });\r\n\r\n//   return ulElement;\r\n// }\r\n\r\nfunction addEventListeners(liElement, todoObj) {\r\n  const dscr = liElement.querySelector(\".task-description\");\r\n  const checkbox = liElement.querySelector(\".completed-checkbox\");\r\n  const trash = liElement.querySelector(\".trash-icon\");\r\n\r\n  checkbox.addEventListener(\"change\", function () {\r\n    dscr.style.textDecoration = this.checked ? \"line-through\" : \"none\";\r\n    todoObj.updateTask(liElement);\r\n  });\r\n\r\n  dscr.addEventListener(\"focus\", function () {\r\n    liElement.style.backgroundColor = \"#ffeea8\";\r\n    this.style.cursor = \"text\";\r\n  });\r\n\r\n  dscr.addEventListener(\"focusout\", function () {\r\n    liElement.style.backgroundColor = \"transparent\";\r\n    this.style.cursor = \"default\";\r\n    if (this.value.trim() === \"\") {\r\n      todoObj.removeTask(liElement);\r\n    } else {\r\n      todoObj.updateTask(liElement);\r\n    }\r\n  });\r\n\r\n  dscr.addEventListener(\"keydown\", (e) => {\r\n    if (e.keyCode === 13) {\r\n      e.target.blur();\r\n    }\r\n  });\r\n\r\n  trash.addEventListener(\"click\", () => {\r\n    todoObj.removeTask(liElement);\r\n  });\r\n}\n\n//# sourceURL=webpack://to_do_list/./src/modules/ul.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/define property getters */
@@ -90,7 +59,8 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/modules/ul.js");
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/modules/ul.js"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
